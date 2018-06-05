10a6fd78957f1916a190b2721e5ef030525f1896
Blacard <blacard@163.com>
2018-06-05 13:21:31 +0800

    优化控制中心

--- src/main/java/cn/virde/susan/control/Control.java
+++ src/main/java/cn/virde/susan/control/Control.java
@@ -26,26 +26,16 @@ public class Control {
 	public void start() {
 		// 控制器开始之前，用户自己需要做一下什么事情
 		option.event.startFunc.done();
-		
-		if(option.getHost()!=null) {
-			Url url = new Url() ;
-			url.setUrl(option.getHost());
-			Thread hostUrlDealThread = new UrlDeal(option,url);
-			hostUrlDealThread.start();
-			try {
-				hostUrlDealThread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}		
-		}
+		// 先处理Host地址
+		getAndDealHostUrlFirst();
 		// 这个地方是有问题的，这里的线程创建速度其实是和数据库的查询性能挂钩的，数据库出数据的速度有多快，线程的创建速度就有多快
 		while(true) {
 			boolean isContinue = cycle();
 			if(!isContinue) {
 				option.event.endFunc.done();
-				System.out.println("program is end");
 				break ;
 			}
+			sleepByOption();
 		}
 	}
 	
@@ -57,20 +47,20 @@ public class Control {
 	 */
 	public boolean cycle() {
 		List<Url> list = getExtractUrl() ;
+		if(list == null ) return false ;
 		
-		if(list == null ) {
-			return false ;
-		}
 		for(Url url : list) {
 			new UrlDeal(option,url).start();
-		}
-
-		if(option.getSleepTime() != 0 ) {
-//			Thread.sleep(option.getSleepTime());
 		}
 		return true ;
 	}
 	
+	/**
+	 * 
+	 * @author Virde
+	 * @date 2018年6月5日 下午1:15:03
+	 * @return
+	 */
 	private List<Url> getExtractUrl(){		
 		try {
 			long start = System.currentTimeMillis() ;
@@ -82,6 +72,29 @@ public class Control {
 			Log.error("控制器在查询待爬取链接时数据库可能出现异常，将在10秒后重新尝试查询", e);
 			try {Thread.sleep(10_000);} catch (InterruptedException e1) {e1.printStackTrace();}
 			return getExtractUrl() ;
+		}
+	}
+	
+	private void getAndDealHostUrlFirst() {
+		if(option.getHost()!=null) {
+			Url url = new Url() ;
+			url.setUrl(option.getHost());
+			Thread hostUrlDealThread = new UrlDeal(option,url);
+			hostUrlDealThread.start();
+			try {
+				hostUrlDealThread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}		
+		}
+	}
+	private void sleepByOption() {
+		if(option.getSleepTime() != 0 ) {
+			try {
+				Thread.sleep(option.getSleepTime());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
